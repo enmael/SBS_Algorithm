2665b747decb12136ba87b9db4540aacadb4e76c
enmael25 <81508671+enmael@users.noreply.github.com>
2024-07-24 15:45:55 +0900

    2024.07.24(백트래킹)

---
+++ Class16th (euclideanAlgorithm)/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace Class16th__euclideanAlgorithm_
+{
+    internal class Program
+    {
+        #region 최대 공략수
+
+        static int greatestCommonDivisor(int x, int y)
+        {
+            int n = 2;
+            int i = 0;
+
+            int[] arrray = new int[10];
+
+            while (x >= n && y >= n)
+            {
+                if (x % n == 0 && y % n == 0)
+                {
+                    arrray[i] = n;
+                    i++;
+                    n++;
+                }
+                else
+                {
+                    n++;
+                }
+            }
+
+            int number = 0;
+
+            for (int j = 0; j < arrray.Length; j++)
+            {
+                if (number < arrray[j])
+                {
+                    number = arrray[j];
+                }
+            }
+
+            return number;
+        }
+
+        static int greatestCommonDivisor2(int x, int y)
+        {
+            int n = 2;
+            int i = 0;
+            int number = 0;
+
+            while (x >= n && y >= n)
+            {
+                if (x % n == 0 && y % n == 0)
+                {
+                    number = n;
+                    i++;
+                    n++;
+                }
+                else
+                {
+                    n++;
+                }
+            }
+
+            return number;
+        }
+
+        #endregion
+
+
+        #region 유클리드 호제법
+        //Euclidean algorithm
+        static int euclideanAlgorithm(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int c = 0;
+
+
+            c = a % b;
+
+
+            if (c != 0 && b % c == 0)
+            {
+                return c;
+            }
+            else if (c == 0)
+            {
+                return b;
+            }
+            else
+            {
+                return euclideanAlgorithm(b, c);
+            }
+
+        }
+
+        #endregion
+        static void Main(string[] args)
+        {
+            #region 유쿨라두 호제법
+
+            //두개의 자연수 또는 정식의 최대 공략수를 구하는 알고리즘으로 
+            // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘 입니다.
+
+            #region 최대 공략수
+
+            //Console.WriteLine(greatestCommonDivisor2(5, 15));
+            //Console.WriteLine(greatestCommonDivisor2(30, 48));
+
+            #endregion
+            //Console.WriteLine(euclideanAlgorithm(24, 18));
+            //Console.WriteLine(euclideanAlgorithm(24, 18));
+            //Console.WriteLine(euclideanAlgorithm(78696, 19332));
+            //Console.WriteLine(euclideanAlgorithm(48, 30));
+            Console.WriteLine(euclideanAlgorithm(15, 5));
+            //Console.WriteLine(78696 % 19332);
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,17 +1,37 @@
-﻿using System.ComponentModel;
-using System.Data;
-
-namespace Program
+﻿namespace Program
 {
-
-
+ 
     internal class Program
     {
-       
+        #region 백트래킹(하노이탑)
 
+        static void Hanoi(int n, char from, char by, char to)
+        {
+            
+        }
+
+        static void Hanoi2(int n, int[]from, int[]by, int[]to)
+        {
+
+            
+        }
+        #endregion
         static void Main(string[] args)
         {
-       
+            #region 백트래킹(하노이탑)
+            // 해를 찾아가는 도중에 지금의 경로가 해가 될것 같지 않으면,
+            //더 이상 깊에 들어가지 않고, 이전 단계로 돌아가는 알고리즘
+
+
+            //백트래킹의 유망성 판단
+            //해가 될 만하지 판단한 후에 유망하지 않다고 결정된면.
+            //ㄱ, 노드의 이전 노드로 돌아가 다음자식 노드로 이동
+
+            //해가 될만한 가능성이 있다면 유망하다.(promising)
+            //유망하지 않은 노드에 가지 않는것 (pruning)
+
+
+            #endregion
         }
     }
 }
