5b550dfd9cad18653e3a3093602990f5956fb2f5
enmael25 <81508671+enmael@users.noreply.github.com>
2024-07-31 14:09:34 +0900

    2024.07.31(누적합 구간합)

---
+++ Class23th(Prefix Sum)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Class23th_Prefix_Sum_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 누적합
+            /*특정한 배열이 있을때 해당 배열 까지의 합을 의미
+             * 
+             */
+
+            int[] array = new int[] { 7, 3, 4, 5, 1 };
+
+            //int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i <= array.Length - 2)
+                {
+                    array[i + 1] = array[i] + array[i + 1];
+                }
+            }
+
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(j + "번째 " + array[j]);
+            }
+            #endregion
+
+        }
+    }
+}
